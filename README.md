3a500de1977fdac39e50629b04101caf0c779ba7
Richard Wilde <richard@wildesoft.net>
2017-02-19 08:38:49 +0000

    Update for Chester talk

--- README.md
+++ README.md
@@ -7,10 +7,14 @@ View the slides at:-
 
 https://cdn.rawgit.com/rippo/testing.casperjs.presentation.vscode/master/Casper.Mvc/Casper.Mvc/Slides/index.html#1
 
+
+Download phantomjs from http://phantomjs.org/download.html
+(copy it to %APPDATA%\npm)
+
+
 To install (use node npm):-
 ````
 $ npm install casperjs -g
-$ npm install phantomjs -g
 ````
 Edit the following file:-
 ````
@@ -20,6 +24,6 @@ and paste the following code to remove the dependancy on Python and to use the l
 ````
 @ECHO OFF
 CLS
-SET PHANTOMJS_EXECUTABLE=%APPDATA%\npm\node_modules\phantomjs\lib\phantom\bin\phantomjs.exe
+SET PHANTOMJS_EXECUTABLE=%APPDATA%\npm\phantomjs.exe
 %~dp0\node_modules\casperjs\bin\casperjs %*
 ````
--- iis.bat
+++ iis.bat
@@ -1,4 +1,4 @@
 cls
 ::"c:\Program Files\IIS Express\iisexpress.exe" /config:E:\Websites\testing.casperjs.presentation.vscode\Casper.Mvc\.vs\config\applicationhost.config /siteid:1
-::"c:\Program Files\IIS Express\iisexpress.exe" /config:D:\Projects\Presentations\testing.casperjs.presentation.vscode\Casper.Mvc\.vs\config\applicationhost.config /siteid:2
-"c:\Program Files\IIS Express\iisexpress.exe" /config:D:\projects\testing.casperjs.presentation.vscode\Casper.Mvc\.vs\config\applicationhost.config /siteid:3
+"c:\Program Files\IIS Express\iisexpress.exe" /config:D:\Projects\Presentations\testing.casperjs.presentation.vscode\Casper.Mvc\.vs\config\applicationhost.config /siteid:2
+::"c:\Program Files\IIS Express\iisexpress.exe" /config:D:\projects\testing.casperjs.presentation.vscode\Casper.Mvc\.vs\config\applicationhost.config /siteid:3
